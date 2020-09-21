f2f3f3a7d3652bbd05f04b8c9e1ac37d854858e1
Sooyeon <sooyeon.ksy@gmail.com>
2020-09-21 23:23:04 +0900

    remove version print

--- src/main/java/com/kakaobank/webapp/searchplace/SearchplaceApplication.java
+++ src/main/java/com/kakaobank/webapp/searchplace/SearchplaceApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SearchplaceApplication {
     public static void main(String[] args) {
-        String test = org.springframework.core.SpringVersion.getVersion();
-        System.out.println(test);
         SpringApplication.run(SearchplaceApplication.class, args);
     }
 
